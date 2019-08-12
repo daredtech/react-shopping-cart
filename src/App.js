b52b557bfc2b3339279e7e4d35e048b0da50eb28
Red <vhanchar@ucsc.edu>
2019-08-12 15:19:54 -0700

    updated ShoppingCart component to use Context API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Context
 import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 
 function App() {
 	// state properties, 
@@ -24,6 +25,7 @@ function App() {
 	return (
 		<div className="App">
 		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value = {cart}>
 			
 			{/* Navigation */}
 			<Navigation cart={cart} />
@@ -35,7 +37,10 @@ function App() {
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
-
+			{/* <Route
+				path="/cart" component={ShoppingCart}
+			/> */}
+		</CartContext.Provider>
 		</ProductContext.Provider>
 		</div>
 	);

96bafc9183c19689e232b6a89af0951c169b6298
Red <vhanchar@ucsc.edu>
2019-08-12 14:06:34 -0700

    refactored Products component  to use Context API

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import {ProductContext} from './contexts/ProductContext';
+
 function App() {
+	// state properties, 
+	// products keeps track of all available products
+	// cart keeps track of all items in the cart
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
@@ -17,24 +23,20 @@ function App() {
 
 	return (
 		<div className="App">
+		<ProductContext.Provider value={{ products, addItem }}>
+			
+			{/* Navigation */}
 			<Navigation cart={cart} />
-
+			
 			{/* Routes */}
 			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
+				exact path="/" component={Products}/>
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+
+		</ProductContext.Provider>
 		</div>
 	);
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -3,14 +3,21 @@ import React from 'react';
 // Components
 import Product from './Product';
 
-const Products = props => {
+// Context
+import {useContext} from 'react';
+import {ProductContext} from '../contexts/ProductContext';
+
+const Products = () => {
+	// set the state to the value passed by ProductContext
+	const {products, addItem} = useContext(ProductContext);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
